f9dbf2eafe69a54d3811f04803124e5f0668753e
SATYAM NAG <satyamnag642@gmail.com>
2026-05-25 18:40:51 +0530

    removed logo link

--- app/layout.tsx
+++ app/layout.tsx
@@ -72,12 +72,7 @@ export default async function RootLayout({ children }: RootLayoutProps) {
           disableTransitionOnChange
         >
           <header className="fixed top-0 left-0 z-50 hidden w-full flex-row justify-between p-6 md:flex">
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://tutrtalk.vercel.app/"
-              className="scale-100 transition-transform duration-300 hover:scale-110"
-            >
+            <div className="scale-100 transition-transform duration-300 hover:scale-110">
               {/* eslint-disable-next-line @next/next/no-img-element */}
               <img src={logo} alt={`${companyName} Logo`} className="block size-6 dark:hidden" />
               {/* eslint-disable-next-line @next/next/no-img-element */}
@@ -86,7 +81,7 @@ export default async function RootLayout({ children }: RootLayoutProps) {
                 alt={`${companyName} Logo`}
                 className="hidden size-6 dark:block"
               />
-            </a>
+            </div>
           </header>
 
           {children}
