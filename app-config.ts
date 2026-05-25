93f1710aca65b96fd0ebc6e835f0a2709c50f5e0
SATYAM NAG <satyamnag642@gmail.com>
2026-05-25 18:18:19 +0530

    updated frontend

--- app-config.ts
+++ app-config.ts
@@ -42,9 +42,9 @@ export const APP_CONFIG_DEFAULTS: AppConfig = {
   supportsScreenShare: false,
   isPreConnectBufferEnabled: true,
 
-  logo: '/tutrtalk-logo.png',
+  logo: '/tutrtalk-logo-light.png',
   accent: '#002cf2',
-  logoDark: '/tutrtalk-logo.png',
+  logoDark: '/tutrtalk-logo-dark.png',
   accentDark: '#1fd5f9',
   startButtonText: 'Start Now',
 
