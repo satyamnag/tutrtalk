ff96476c9cd2bda745e5a0e30b16d6497339bba3
SATYAM NAG <satyamnag642@gmail.com>
2026-06-02 16:44:24 +0530

    frontend for light mode

--- app-config.ts
+++ app-config.ts
@@ -42,9 +42,9 @@ export const APP_CONFIG_DEFAULTS: AppConfig = {
   supportsScreenShare: false,
   isPreConnectBufferEnabled: true,
 
-  logo: '/tutrtalk-logo-dark_light.png',
+  logo: '/tutrtalk-logo-light.png',
   accent: '#1fd5f9',
-  logoDark: '/tutrtalk-logo-dark_light.png',
+  logoDark: '/tutrtalk-logo-dark.png',
   accentDark: '#1fd5f9',
   startButtonText: 'Start Now',
 
