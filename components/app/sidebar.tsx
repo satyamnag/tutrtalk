7ec27ece9502318e56885605313338e264250fda
SATYAM NAG <satyamnag642@gmail.com>
2026-06-03 23:31:10 +0530

    updated left side bar close icon

--- components/app/sidebar.tsx
+++ components/app/sidebar.tsx
@@ -14,6 +14,7 @@ import {
   SettingsIcon,
   PanelLeftOpenIcon,
   PanelLeftCloseIcon,
+  MenuIcon,
 } from 'lucide-react';
 import { cn } from '@/lib/shadcn/utils';
 
@@ -64,7 +65,7 @@ export function SidebarToggle() {
   const { open, setOpen } = useSidebar();
   const { isLoaded, isSignedIn } = useUser();
 
-  const Icon = open ? PanelLeftCloseIcon : PanelLeftOpenIcon;
+  const Icon = open ? PanelLeftCloseIcon : MenuIcon;
 
   return (
     <div className="h-10 w-10 flex items-center justify-center">
